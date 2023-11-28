f1491a0ae3e3268d2be69c66385df7203b826741
Haris Ahmad <harizahmad100@gmail.com>
2023-11-28 18:36:51 -0500

    added model for request

---
+++ backend/src/main/java/edu/psu/backend/model/Request.java
@@ -0,0 +1,21 @@
+package edu.psu.backend.model;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "requests")
+public class Request {
+
+    @Id
+    private String id;
+    private String tenantId;
+    private int apartmentNum;
+    private String location;
+    private String description;
+    private String status;
+    private Date requestTime;
+    private String image;
+
+}
